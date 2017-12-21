15844ce70472bf4c479935363cbc82e18b731398
mamba <h.hochart29@gmail.com>
2017-12-21 01:18:38 +0100

    singleton instance not working

--- src/front/device.js
+++ src/front/device.js
@@ -1,62 +1,60 @@
-let deltaY;
-let startX;
-let startY;
-let x;
-let y;
-let deltaX;
-let startTime;
-let endTime;
-let deltaTime;
-let speed;
-let angle;
+let deltaY
+let startX
+let startY
+let x
+let y
+let deltaX
+let startTime
+let endTime
+let deltaTime
+let speed
+let angle
 
-let data = {};
+let data = {}
 
-// import io from 'socket.io-client';
+import socket from './socket'
 
-// const socket = io('http://172.16.61.66:1337/');
-// socket.on('connect', (client) => {
-
+console.log(socket.instance.setConnection())
 //On appel chaque évènement liés au touché de l'écran
-    window.addEventListener("touchstart", handleStart, false);
-    window.addEventListener("touchend", handleEnd, false);
-    window.addEventListener("touchmove", handleMove, false);
+window.addEventListener('touchstart', handleStart, false)
+window.addEventListener('touchend', handleEnd, false)
+window.addEventListener('touchmove', handleMove, false)
 
 //Fonction appelée lors du touché de l'écran
-    function handleStart(event) {
-        startTime = new Date().getTime();
-        startY = event.touches[0].pageY;
-        startX = event.touches[0].pageX;
-    }
+function handleStart (event) {
+  startTime = new Date().getTime()
+  startY = event.touches[0].pageY
+  startX = event.touches[0].pageX
+}
 
 //Fonction appelée lors de la fin du touché de l'écran
-    function handleEnd(event) {
-        endTime = new Date().getTime();
-        deltaTime = endTime - startTime;
-        speed = deltaY / deltaTime;
-        speed = Math.floor(speed * 10);
-        data.touchSpeed = speed;
+function handleEnd (event) {
+  endTime = new Date().getTime()
+  deltaTime = endTime - startTime
+  speed = deltaY / deltaTime
+  speed = Math.floor(speed * 10)
+  data.touchSpeed = speed
 
-        /*
-        * Emit ici le data
-        * */
-        socket.emit('clientEmitData', data);
-        console.log(data);
+  /*
+  * Emit ici le data
+  * */
+  socketConnection.c.emit('clientEmitData', data)
+  console.log(data)
 
-        document.getElementById("touchSpeed").innerHTML = "Speed : " + data.touchSpeed;
-    }
+  document.getElementById('touchSpeed').innerHTML = 'Speed : ' + data.touchSpeed
+}
 
 //Fonction appelé lors d'un mouvement sur l'écran
-    function handleMove(event) {
-        x = event.touches[0].pageX;
-        y = event.touches[0].pageY;
-        deltaX = startX - x;
-        deltaY = startY - y;
-        if (deltaY < 0) {
-            deltaY = -deltaY;
-        }
-        angle = Math.sin(deltaX / deltaY) * 180 / Math.PI;
-        angle = angle.toFixed(2);
-        data.touchAngle = angle;
-        document.getElementById("touchAngle").innerHTML = "Angle : " + data.touchAngle;
-    }
+function handleMove (event) {
+  x = event.touches[0].pageX
+  y = event.touches[0].pageY
+  deltaX = startX - x
+  deltaY = startY - y
+  if (deltaY < 0) {
+    deltaY = -deltaY
+  }
+  angle = Math.sin(deltaX / deltaY) * 180 / Math.PI
+  angle = angle.toFixed(2)
+  data.touchAngle = angle
+  document.getElementById('touchAngle').innerHTML = 'Angle : ' + data.touchAngle
+}
--- src/front/main.js
+++ src/front/main.js
@@ -1,32 +1,30 @@
 require('./styles/main.scss')
 
-import io from 'socket.io-client'
+import socket from './socket'
+console.log(socket.instance.setConnection())
 
-window.socket = io('http://192.168.0.11:1337') //home
-// const socket = io('http://192.168.1.27:1337');
-
-window.socket.on('connect', (client) => {
-  window.socket.emit('emitURL', window.location.pathname)
-
-  let type = ''
-  if (navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i)) {
-    type = 'device'
-  } else {
-    type = 'desktop'
-  }
-  window.socket.emit('clientConnect', {id: window.socket.id, type: type})
-})
-
-/*
-* Debug to display informations about a client / clients
-*/
-window.socket.on('displayClientInfo', (clientDebug) => {
-  console.log(clientDebug)
-})
-
-/*
-* Redirect to the desired URL
-* */
-window.socket.on('redirect', (url) => {
-  window.location.href = window.location.origin + url
-})
+// socketConnection.c.on('connect', (client) => {
+//   socketConnection.c.emit('emitURL', window.location.pathname)
+//
+//   let type = ''
+//   if (navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i)) {
+//     type = 'device'
+//   } else {
+//     type = 'desktop'
+//   }
+//   socketConnection.c.emit('clientConnect', {id: socketConnection.c.id, type: type})
+// })
+//
+// /*
+// * Debug to display informations about a client / clients
+// */
+// socketConnection.c.on('displayClientInfo', (clientDebug) => {
+//   console.log(clientDebug)
+// })
+//
+// /*
+// * Redirect to the desired URL
+// * */
+// socketConnection.c.on('redirect', (url) => {
+//   window.location.href = window.location.origin + url
+// })
