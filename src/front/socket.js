bcac2ce4251880d400ee4afbe7ce073972258cf2
mamba <h.hochart29@gmail.com>
2018-01-09 23:13:16 +0100

    bug ordre script dans le build

--- src/front/socket.js
+++ src/front/socket.js
@@ -1,3 +1,3 @@
 import io from 'socket.io-client'
 
-export const socketConnection = io('http://192.168.1.15:1337')
+export const socketConnection = io('http://192.168.0.11:1337')
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,25 @@
 const path = require('path')
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 const plugins = []
 
 if (process.env.NODE_ENV === 'production') {
   plugins.push(new UglifyJSPlugin())
+  plugins.push(new HtmlWebpackPlugin({
+    title: 'Sockanvas - BeerPong interactif'
+  }))
 }
 
 module.exports = {
   entry: {
+    socket: ['./src/front/socket.js'],
     bundle: ['./src/front/main.js'],
-    device: ['./src/front/device.js'],
-    socket: ['./src/front/socket.js']
+    device: ['./src/front/device.js']
   },
   devtool: 'eval-source-map',
   output: {
-    filename: '[name].js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
